825a588e29f3f27c2906c66b64bd02323efcc41f
Nikodem Czarlinski <nczarlinski@gmail.com>
2022-11-20 22:05:41 +0000

    Put swin files into folder and discard focal loss increase threshold boxes

--- configs/Swin/base_swin_1.py
+++ configs/Swin/base_swin_1.py
@@ -6,7 +6,6 @@ _base_ = [
     f"{mmdet_base}/default_runtime.py",
 ]
 
-
 pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
 model = dict(
     type='MaskRCNN',
